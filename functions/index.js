57651a56ab531d1091f6043558bf1a90116c47a8
ayjzcoder <102896985+ayjzcoder@users.noreply.github.com>
2023-09-12 09:02:14 +0300

    fixed index.ejs

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req,res)=> {
-    res.render("index.ejs")
+  var processes = [];
+
+  res.render("index", {
+    processes: processes,
+    option: "FCFS",
+    inputA: req.body.arrivalTimes,
+    inputB: req.body.burstTimes,
+    ganttChart: processes,
+    averages: processes,
+  });
+  //res.render("index.ejs");
 })
 
 app.listen(3000, ()=>{
